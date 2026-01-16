c9c6536af1a3d0159f1f598cafc310cf681c3e81
Shakib Khan <leadwithshakib@gmail.com>
2026-01-16 12:07:15 +0600

    Update layout with ThemeProvider and Toaster, extend Prisma schema for Better Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+            {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
